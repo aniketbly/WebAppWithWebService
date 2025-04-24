d0026248853e8bbe1c5e1b14db633e724f93622c
Aniket <aniketbly@gmail.com>
2025-04-24 21:12:44 +0530

    ver 1

---
+++ WebApplication1/WebApplication1/Home.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="WebApplication1.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Hello Time is:-</h1>
+
+            <asp:Label runat="server" ID="lblTime"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebApplication1/Home.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            WebApplication1.WebService1 obj = new WebService1();
+            
+            lblTime.Text = obj.GetTime();
+        }
+    }
+}
